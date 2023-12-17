7fc28602fe41aa7fda915a5523700b2abdc222f6
parthMalhotra007 <parthmalhotra.me@gmail.com>
2023-12-17 18:28:52 +0530

    https://leetcode.com/problems/reverse-linked-list/description/

---
+++ ReverseLinkedList.java
@@ -0,0 +1,25 @@
+public class ReverseLinkedList {
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev =null;
+        ListNode curr = head;
+        ListNode next =null;
+
+        while(curr != null)
+        {
+            next = curr.next;
+            curr.next =prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
