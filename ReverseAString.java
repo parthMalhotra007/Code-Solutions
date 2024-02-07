f29dccb72f2115e2b84c5485c34fdfb7f5244c3b
Parth Malhotra <parthMalhotra.me@gmail.com>
2024-02-08 02:33:59 +0530

    https://www.geeksforgeeks.org/reverse-words-in-a-given-string/

---
+++ ReverseAString.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+
+public class ReverseAString {
+
+
+    public String reverse(String input)
+    {
+        String[] substrings = input.split(" ");
+        Stack<String> stack = new Stack<>();
+        stack.addAll(Arrays.asList(substrings));
+        StringBuilder stringBuilder = new StringBuilder();
+        while(!stack.isEmpty()) {
+            stringBuilder.append(stack.pop());
+            stringBuilder.append(" ");
+        }
+
+        return stringBuilder.toString();
+
+    }
+
+    public  String reverseEff(String input) // Constant time and efficient
+    {
+        String[] substrings = input.split(" ");
+        Collections.reverse(Arrays.asList(substrings));
+        return String.join(" " , substrings);
+    }
+
+    public static void main(String[] args) {
+        String s = "i love programming very much";
+        ReverseAString reverseAString = new ReverseAString();
+        String res = reverseAString.reverseEff(s);
+        System.out.println(res);
+
+    }
+}
